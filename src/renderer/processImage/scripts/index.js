9f3d3ebe670aa4e08523df474ddf9dfc9ddd2093
Vitor Santos <vitorsantoo134@gmail.com>
2022-09-28 18:00:27 -0300

    negative fixed

--- src/renderer/processImage/scripts/index.js
+++ src/renderer/processImage/scripts/index.js
@@ -14,38 +14,39 @@ class ImageProcessing {
     const script = join(__dirname, filterPath);
     const result = PythonShell.run(script, optionsPath,
       function (err, results) {
-        // if (err) throw err;
+        if (err) {
+          return console.log(err.traceback)
+        }
         console.log('finished');
-        console.log(results);
 
-        // let win = new BrowserWindow({width: 800, height: 600,webPreferences: {
-        //   nodeIntegration: true,
-        //   contextIsolation: false,
-        //   webSecurity: true
-        // },}); 
-        // const images  =  []
-        // const nameImage = results?.pop()
-        // const resultJSON = JSON.parse(results[0])
-        // resultJSON.forEach(function ({url,name}){
-        //   const dataImage = fs.readFileSync(path.resolve(__dirname,'..','..','..','..',url)).toString('base64')
-        //   images.push({url: `data:image/png;base64,${dataImage}`, name})
-        // })
+        let win = new BrowserWindow({width: 800, height: 600,webPreferences: {
+          nodeIntegration: true,
+          contextIsolation: false,
+          webSecurity: true
+        },}); 
+        const images  =  []
+        const nameImage = results?.pop()
+        const resultJSON = JSON.parse(results[0])
+        resultJSON.forEach(function ({url,name}){
+          const dataImage = fs.readFileSync(path.resolve(__dirname,'..','..','..','..',url)).toString('base64')
+          images.push({url: `data:image/png;base64,${dataImage}`, name})
+        })
         
-        // ejs.renderFile(path.resolve(__dirname,'..','..','templates','index.ejs'), {
-        //   images,
-        //   nameImage
-        // }, {}, function (err, str) {
-        //   if (err) {
-        //     console.log(err);
-        //   }
-        //   // Load the rendered HTML to the BrowserWindow.
-        //   win.loadURL('data:text/html;charset=utf-8,' + encodeURI(str));
-        // });
+        ejs.renderFile(path.resolve(__dirname,'..','..','templates','index.ejs'), {
+          images,
+          nameImage
+        }, {}, function (err, str) {
+          if (err) {
+            console.log(err);
+          }
+          // Load the rendered HTML to the BrowserWindow.
+          win.loadURL('data:text/html;charset=utf-8,' + encodeURI(str));
+        });
         
-        // return results[0];
+        return results[0];
       }
     );
-    // return result;
+    return result;
   }
 
   // logarithmic(filePath, logImageName) {
